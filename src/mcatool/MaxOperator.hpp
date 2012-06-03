4710650bd1d677f8ca7747d71238fb8fcde8cd6b
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2012-06-03 19:53:39 +0200

    Added license header to all source files.

--- src/mcatool/MaxOperator.hpp
+++ src/mcatool/MaxOperator.hpp
@@ -1,4 +1,24 @@
-
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of pnitools.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ * Created on: 03.06.2012
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
 #ifndef __MAXOPERATOR_HPP__
 #define __MAXOPERATOR_HPP__
 
@@ -17,14 +37,17 @@ class MaxOperator:public Operator
         size_t _max_pos;
         Float64 _max_value;
     public:
+        //---------------------------------------------------------------------
         MaxOperator(const po::variables_map &config):
             Operator(config),
             _max_pos(0),
             _max_value(0)
         {}
 
+        //---------------------------------------------------------------------
         ~MaxOperator(){}
 
+        //---------------------------------------------------------------------
         virtual void operator()(const Float64Array &channels,
                                 const Float64Array &data)
         {
@@ -32,6 +55,7 @@ class MaxOperator:public Operator
             _max_value = data[_max_pos];
         }
 
+        //---------------------------------------------------------------------
         virtual std::ostream &stream_result(std::ostream &o) const
         {
             o<<_max_pos<<"\t"<<_max_value;
--- src/mcatool/Operator.hpp
+++ src/mcatool/Operator.hpp
@@ -1,4 +1,24 @@
-
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of pnitools.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ * Created on: 03.06.2012
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
 #ifndef __OPERATOR_HPP__
 #define __OPERATOR_HPP__
 
@@ -14,15 +34,21 @@ namespace po = boost::program_options;
 class Operator
 {
     public:
+        //---------------------------------------------------------------------
         Operator(const po::variables_map &config){}
+
+        //---------------------------------------------------------------------
         virtual ~Operator() {}
 
+        //---------------------------------------------------------------------
         virtual void operator()(const Float64Array &channels,
                         const Float64Array &data) = 0;
 
+        //---------------------------------------------------------------------
         virtual std::ostream &stream_result(std::ostream &o) const = 0;
 };
 
+//-----------------------------------------------------------------------------
 std::ostream &operator<<(std::ostream &o,const Operator &op)
 {
     return op.stream_result(o);
