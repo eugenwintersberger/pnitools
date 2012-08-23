ba9c59c7b64284927b87a6694863194b21ec7c01
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-08-23 17:05:15 +0200

    Added TIFF support (not tested yet) to detinfo. Fixed th license header in detinfos main.cpp. Fixed license header in mcaops main.cpp.

--- src/detinfo/main.cpp
+++ src/detinfo/main.cpp
@@ -1,8 +1,23 @@
 /*
- * main.cpp
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
  *
- *  Created on: Sep 26, 2011
- *      Author: Eugen Wintersberger
+ * This file is part of pnitools.
+ *
+ * pnitools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * pnitools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ * Created on: 23.08.2012
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
  */
 
 #include<iostream>
@@ -75,6 +90,8 @@ int main(int argc,char **argv)
         }
         else if(file.extension()==".tiff")
         {
+            pni::io::TIFFReader reader(file.path());
+            info = reader.info(0);
 
         }
         else
@@ -92,8 +109,6 @@ int main(int argc,char **argv)
         std::cout<<" type = "<<info.get_channel(0).type_id()<<std::endl;
 
     }
-
-
 
     return 0;
 }
