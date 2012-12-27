ef8be5dd89f21b4ea11ba9298544623b78b1bae8
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2012-12-27 15:21:07 +0100

    Added a class to manage CLI arguments:

--- src/common/cli_args.cpp
+++ src/common/cli_args.cpp
@@ -1,21 +1,54 @@
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of pnitools.
+ *
+ * pnitools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * pnitools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ * Created on: 19.12.2012
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+
 #include "cli_args.hpp"
 
 //-----------------------------------------------------------------------------
 cli_args::~cli_args()
 {
-    _str_values.clear();
-    if(_str_ptrs) delete [] _str_ptrs;
-    _str_ptrs = nullptr;
+    for(size_t i = 0;i<_argc;++i)
+        if(_argv[i]) delete [] _argv[i];
+
+    if(_argv) delete [] _argv;
+    _argc = 0;
+    _argv = nullptr;
 }
 
 //-----------------------------------------------------------------------------
 const char **cli_args::argv() const
 {
-    return (const char **)_str_ptrs;
+    return (const char **)_argv;
 }
 
 //-----------------------------------------------------------------------------
 int cli_args::argc() const
 {
-    return _str_values.size();
+    return _argc;
+}
+
+//-----------------------------------------------------------------------------
+std::ostream &operator<<(std::ostream &o,const cli_args &args)
+{
+    for(size_t i=0;i<args.argc();i++) o<<args.argv()[i]<<std::endl;
+
+    return o;
 }
--- src/common/cli_args.hpp
+++ src/common/cli_args.hpp
@@ -1,7 +1,34 @@
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of pnitools.
+ *
+ * pnitools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * pnitools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ * Created on: 19.12.2012
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
 #pragma once
 
+#include <iostream>
 #include <vector>
 #include <pni/utils/Types.hpp>
+
+extern "C"
+{
+#include<string.h>
+}
 
 using namespace pni::utils;
 
@@ -9,32 +36,70 @@ using namespace pni::utils;
 \brief class managing CLI options
 
 This class is used to hold CLI options in a form convineant for the
-program_options class.
+program_options class. The intention of this class is to act as a exception save
+container for custom built command line options that are generated from a
+container of strings. The content of this string is than presented in way that
+the program options class can parse it.
+\code
+std::list<std::string> strlist;
+
+......code omitted .....
+
+cli_args args(strlist);
+program_config config;
+
+......setup of config is omitted ....
+
+config.parse(args.argc(),const_cast<char**>(args.argv()));
+\endcode
+Instances of cli_args are first class objects and do memory management by
+themselfe. 
 */
 class cli_args
 {
     private:
-        std::vector<String> _str_values;
-        char **_str_ptrs;
+        //! number of strings in the array below
+        int _argc;
+        //! pointers to the strings content
+        char **_argv;
          
     public:
+        //---------------------------------------------------------------------
+        //! constructor 
         template<typename CTYPE> cli_args(const CTYPE &str_cont);
+
+        //---------------------------------------------------------------------
+        //! destructor
         ~cli_args();
-        const char **argv() const;
+
+        //---------------------------------------------------------------------
+        //! get the argument values
+        const char **argv()const;
+
+        //---------------------------------------------------------------------
+        //! get the number of arguments
         int argc() const;
 };
 
 //-----------------------------------------------------------------------------
 template<typename CTYPE> cli_args::cli_args(const CTYPE &str_cont):
-    _str_values(str_cont.size()),
-    _str_ptrs(new char*[str_cont.size()])
+    _argc(str_cont.size()),
+    _argv(new char*[str_cont.size()])
 {
-    //copy original data
-    std::copy(str_cont.begin(),str_cont.end(),_str_values.begin());
-
-    //set the pointers 
     size_t index = 0;
-    for(auto v: _str_values)
-        _str_ptrs[index++] = v.c_str();
+    for(auto v: str_cont)
+    {
+        //allocate memory 
+        _argv[index] = new char[v.size()+1];
+
+        //copy string
+        strncpy(_argv[index],v.c_str(),v.size()+1); 
+        
+        index++; //increment argument index
+    }
 
 }
+
+//-----------------------------------------------------------------------------
+//! output operator
+std::ostream &operator<<(std::ostream &ostr,const cli_args &args);
