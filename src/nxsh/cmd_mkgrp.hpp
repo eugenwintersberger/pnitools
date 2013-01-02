bbf52c1e9e69a0ee8c2b10bc4a89f99050ea1af6
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-01-02 13:54:50 +0100

    Fixed documentation and license.

--- src/nxsh/cmd_mkgrp.hpp
+++ src/nxsh/cmd_mkgrp.hpp
@@ -1,12 +1,40 @@
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of pnitools.
+ *
+ * pnitools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * pnitools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ * Created on: Jan 02, 2013
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
 #pragma once
 
 #include "command.hpp"
 
+/*!
+\brief create a group
+
+Create a group. The command takes one argument: the group to create. 
+The only option allowed is -c (--class) a string determining the class of the
+group. 
+*/
 class cmd_mkgrp : public command
 {
     private:
-        String _class;
-        String _name;
+        String _class; //!< class name
+        String _name;  //!< group name 
     public:
         virtual void setup(const std::vector<String> &cargs);
         virtual void execute(std::unique_ptr<environment> &env);
