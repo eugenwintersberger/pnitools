f56e615829f0c7ab06f58c8f5a875d8eee53ac54
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-04-26 16:34:36 +0200

    Fixed issues with nxtee

--- src/nxtee/main.cpp
+++ src/nxtee/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
+#include <fstream>
 
 #include <pni/core/config/config_parser.hpp>
 #include <pni/core/config/configuration.hpp>
 
 #include <pni/io/nx/nx.hpp>
 
-#include "../commong/nx_object_path.hpp"
+#include "../common/nx_object_path.hpp"
 
 using namespace pni::core;
 using namespace pni::io::nx::h5;
+
+template<typename STREAMT> void read_stream(STREAMT &stream)
+{
+    while(!stream.eof())
+    {
+        string buffer;
+        stream>>buffer;
+        std::cout<<buffer;
+    }
+}
 
 int main(int argc,char **argv)
 {
     configuration config;
 
     config.add_argument(config_argument<nx_object_path>("object-path",-1));
+    config.add_option(config_option<string>("input","i","input file",""));
+    config.add_option(config_option<string>("comment","c","comment sign",""));
+    config.add_option(config_option<string>("delimiter","d","column delimiter"," "));
     config.add_option(config_option<bool>("help","h","show help",false));
 
     parse(config,cliargs2vector(argc,argv));
@@ -28,11 +42,17 @@ int main(int argc,char **argv)
 
         return 1;
     }
+    
+    if(config.value<string>("input")!="")
+    {
+        //open data file
+        std::ifstream input_stream(config.value<string>("input")); 
+        read_stream(input_stream);
+    }
+    else
+        read_stream(std::cin);
 
-    //read data from stdinput
-    std::istream input_stream(std::cin);
 
-    while(input_stream
 
     return 0;
 }
