0a9e8a69ecd57fd902fe36ca5e1af52f71f1ef7f
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-10-18 16:58:01 +0200

    Fixed certificate check for unsecure connections

--- test/data/download.sh
+++ test/data/download.sh
@@ -4,5 +4,5 @@ FILE_ID="0B3ylb_x4-XOgUG1XZkgtcTJOZjA"
 FILE_NAME="pnitools-test-data.tar.xz"
 HOST_URL="https://googledrive.com/host/"
 
-wget -O$FILE_NAME "$HOST_URL$FILE_ID"
+wget --no-check-certificate -O$FILE_NAME "$HOST_URL$FILE_ID"
 
