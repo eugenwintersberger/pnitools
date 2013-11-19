ca3d90cfb2c576e192ebd5c931152a6dc85e0ef8
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-11-19 12:11:48 +0100

    Updated download script for new test data.

--- test/data/download.sh
+++ test/data/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-FILE_ID="0B3ylb_x4-XOgUG1XZkgtcTJOZjA"
-FILE_NAME="pnitools-test-data.tar.xz"
+FILE_ID="0B3ylb_x4-XOgZ1duOWRQV2R1cGc"
+FILE_NAME="pnitools-test-data.tar.gz"
 HOST_URL="https://googledrive.com/host/"
 
 wget --no-check-certificate -O$FILE_NAME "$HOST_URL$FILE_ID"
