776eae545f972a4055f7a03a89eadfd1e3eb4a56
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2014-10-07 15:37:29 +0200

    Added functions to string_utils
    Added to_lower and to_upper functions to string_utils.
    
    Update issue 2

--- src/common/string_utils.cpp
+++ src/common/string_utils.cpp
@@ -1,25 +1,26 @@
-/*
- * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
- *
- * This file is part of pnitools.
- *
- * pnitools is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * pnitools is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
- *************************************************************************
- * Created on: Oct 18, 2012
- *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
- */
+//
+// (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of pnitools.
+//
+// pnitools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// pnitools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+// Created on: Oct 18, 2012
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
 
+#include <cctype>
 #include "string_utils.hpp"
 
 //-----------------------------------------------------------------------------
@@ -48,5 +49,22 @@ string trim(const string &s)
     }
 
     return string(s,start_index,(s.size()-stop_index-start_index));
+}
+
+//----------------------------------------------------------------------------
+string to_lower(const string &s)
+{
+    string l(s);
+    std::transform(s.begin(),s.end(),l.begin(),::tolower);
+    return l;
+}
+
+//----------------------------------------------------------------------------
+string to_upper(const string &s)
+{
+    string u(s);
+    
+    std::transform(s.begin(),s.end(),u.begin(),::toupper);
+    return u;
 }
 
--- src/common/string_utils.hpp
+++ src/common/string_utils.hpp
@@ -1,55 +1,75 @@
-/*
- * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
- *
- * This file is part of pnitools.
- *
- * pnitools is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * pnitools is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
- *************************************************************************
- * Created on: Oct 18, 2012
- *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
- */
+//
+// (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of pnitools.
+//
+// pnitools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// pnitools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+// Created on: Oct 18, 2012
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
 
 #pragma once
 #include <boost/regex.hpp>
 #include <boost/lexical_cast.hpp>
 #include <pni/core/types.hpp>
 
-#ifdef NOFOREACH
-#include <boost/foreach.hpp>
-#endif
-
 using namespace pni::core;
 
-/*!
-\brief trim a string
-
-Remove leading and trailing blanks from a string.
-\param s string to trim
-\return trimmed string
-*/
+//!
+//! \brief trim a string
+//! 
+//! Remove leading and trailing blanks from a string.
+//! \param s string to trim
+//! \return trimmed string
+//!
 string trim(const string &s);
 
-//-----------------------------------------------------------------------------
-/*!
-\brief read shape 
+//----------------------------------------------------------------------------
+//!
+//! \brief make lower-case string
+//! 
+//! Convert the original string to lower case letters.
+//! 
+//! \param s input string
+//! \return s in lower case characters
+//!
+string to_lower(const string &s);
 
-Read a shape from a string and store the result in a container defined by the
-user. 
-\tparam STYPE STL compliant container type
-\param s string from which to construct the shape
-\return container with shape data
-*/
+//----------------------------------------------------------------------------
+//!
+//! \brief make upper-case string
+//! 
+//! Convert the original string to upper case letters.
+//! 
+//! \param s input string
+//! \return s in upper case characters
+//!
+string to_upper(const string &s);
+
+
+//-----------------------------------------------------------------------------
+//!
+//! \brief read shape 
+//! 
+//! Read a shape from a string and store the result in a container defined 
+//! by the user. 
+//! 
+//! \tparam STYPE STL compliant container type
+//! \param s string from which to construct the shape
+//! \return container with shape data
+//!
 template<typename STYPE> STYPE read_shape(const string &s)
 {
     boost::regex sr("\\d+");
@@ -60,11 +80,7 @@ template<typename STYPE> STYPE read_shape(const string &s)
 
     while(boost::regex_search(start,end,match,sr))
     {
-#ifdef NOFOREACH
-        BOOST_FOREACH(auto value,match)
-#else
         for(auto value: match)
-#endif
             shape.push_back(boost::lexical_cast<typename STYPE::value_type>(value));
 
         start = match[0].second;
