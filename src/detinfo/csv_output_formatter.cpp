66acb48fbbcaa02eba8afb93f8c1e87ee190a550
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2014-10-10 13:23:54 +0200

    Added CSV output to detinfo

---
+++ src/detinfo/csv_output_formatter.cpp
@@ -0,0 +1,53 @@
+//
+// (c) Copyright 2014 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of pnitools.
+//
+// pnitools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// pnitools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+// Created on: Oct 10,2014
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+
+#include "csv_output_formatter.hpp"
+
+const string csv_output_formatter::separator = " ; ";
+
+void csv_output_formatter::header(std::ostream &stream)
+{
+    stream<<"source"<<separator<<"type"<<separator;
+    stream<<"pixel type"<<separator<<"frame shape"<<std::endl;
+}
+
+//----------------------------------------------------------------------------
+void csv_output_formatter::footer(std::ostream &stream)
+{}
+
+//----------------------------------------------------------------------------
+void csv_output_formatter::detector_header(std::ostream &stream)
+{}
+
+//----------------------------------------------------------------------------
+void csv_output_formatter::detector_footer(std::ostream &stream)
+{}
+
+//----------------------------------------------------------------------------
+void csv_output_formatter::write(std::ostream &stream, 
+                                 const detector_info &info)
+{
+    stream<<info.path()<<separator;
+    stream<<string_from_layout(info.layout())<<separator;
+    stream<<info.type_id()<<separator;
+    stream<<info.frame_shape()<<std::endl;
+}
---
+++ src/detinfo/csv_output_formatter.hpp
@@ -0,0 +1,36 @@
+//
+// (c) Copyright 2014 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of pnitools.
+//
+// pnitools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// pnitools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pnitools.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+// Created on: Oct 10,2014
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+#pragma once
+
+#include "output_formatter.hpp"
+
+class csv_output_formatter : public output_formatter
+{
+    static const string separator;
+    public:
+        virtual void header(std::ostream &stream);
+        virtual void footer(std::ostream &stream);
+        virtual void detector_header(std::ostream &stream);
+        virtual void detector_footer(std::ostream &stream);
+        virtual void write(std::ostream &stream,const detector_info &info);
+};
+    
