bc22201f5e08b35ef74353510c5e2625bc110208
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2014-10-14 15:27:50 +0200

    Update of the nxls manpage

--- doc/man/detinfo.1.cmake
+++ doc/man/detinfo.1.cmake
@@ -9,7 +9,6 @@ detinfo - prints detector information from data files
 
 .SH DESCRIPTION
 \fBdetinfo\fR prints information about detector data stored in files. 
-.br
 Currently \fBdetinfo\fR supports the following file formats:
 .RS 3 
 .P
@@ -37,23 +36,14 @@ The following information is provided by \fBdetinfo\fR
 Possible detector types are \fBpoint\fR, \fBlinear\fR, and \fBarea\fR.
 .P 
 For TIF and CBF files \fBdetinfo\fR assumes that all the data in a file belongs
-.br
 to a single detector. However, Nexus files are slightly different as they can 
-.br
 store data from several detectors in a file. In this case, the information
-.br
 listed above is shown for each instance of NXdetector (the group type used to 
-.br
 store data from an individual detector) available in the file.
-.P
 A typical application of \fBdetinfo\fR would be to gather all the information
-.br
 required to merge data from single TIF or CBF files into a Nexus file. 
-.P
 As with all programs from the \fBpni-tools\fR-package input files can be
-.br
 specified as individual files and/or as a file template with a numeric range. 
-.br
 For details see the info page for pni-tools how to do this.
 
 .SH OPTIONS
@@ -83,11 +73,8 @@ Written bei Eugen Wintersberger <eugen.wintersberger@desy.de>
 
 .SH COPYRIGHT
 Copyright 2013 Eugen Wintersberger License GLPLv2+: GNU GPL version 2 or later
-.br
 <http://gnu.org/license/gpl.html>.  This is free software: you are free to
-.br
 change and redistribute it. There is no WARRENTY, to the extent permitted by
-.br
 law.
 
 .SH SEE ALSO
--- doc/man/nxls.1.cmake
+++ doc/man/nxls.1.cmake
@@ -8,12 +8,20 @@ nxls - list content of a Nexus file
 .B nxls [OPTIONS] NEXUSPATH 
 
 .SH DESCRIPTION
-\fBnxls\fR can be understood as the Nexus counterpart of the \fBls\fR on a Unix
-shell. It lists the content of a Nexus file to standard out. \fBnxls\fR takes
-either a single file name or a full Nexus path as input. In the first case the
-content of the root-level of the file is shown. In the latter one all children
-of a given group are shown. See the examples section of this man-page for more
-information about this.
+\fBnxls\fR lists the content of a Nexus file. Like its Unix counter part
+\fBls\fR, \fBnxls\fR shows the content of the file relative to a particular
+group within the file. In the simplest case this is the root group of the file. 
+As the command line argument must be a valid Nexus path in no case the name of
+the file alone will suffice. It is at least necessary to add the root part. 
+In such a case a possible call of \fBnxls\fR would look like this
+.P
+.RS 2
+$ nxls data.nxs://
+.RE
+.P
+By default \fBnxls\fR removes the leading part of the path of the individual 
+Nexus objects. This is just to save space in the output. To obtain the full 
+path use the \fB\-f\fR option.
 
 .SH OPTIONS
 The programm takes the following options:
@@ -26,6 +34,27 @@ show the content of all subgroups of a given Nexus group
 .TP
 \fB\-a\fR,\fB\-\-show\-attributes\fR
 shows attributes in addition to groups and field.
+.TP
+\fB-f\fR, \fB\-\-full\-path\fR
+show the full path for each object
+.TP
+\fB-l\fR, \fB\-\-long\fR
+show additional information for each object
+
+.SH Examples
+.TP
+nxls data.nxs://
+shows the content of the root group
+.TP
+nxls -f data.nxs://:NXentry/:NXinstrument
+lists all objects in the instrument group of the Nexus file with full path
+.TP
+nxls -a data.nxs://:NXentry/:NXinstrument
+lists all objects in the instrument group of the Nexus file including all 
+attributes
+.TP
+nxls -al data.nxs://:NXentry/:NXinstrument/:NXdetector/data
+shows detailed information about the data field in the detector group
 
 .SH EXIT CODE
 0 in the case of success, 1 otherwise
@@ -35,12 +64,16 @@ Written bei Eugen Wintersberger <eugen.wintersberger@desy.de>
 
 .SH COPYRIGHT
 Copyright 2013 Eugen Wintersberger License GLPLv2+: GNU GPL version 2 or later
+.br
 <http://gnu.org/license/gpl.html>.  This is free software: you are free to
+.br
 change and redistribute it. There is no WARRENTY, to the extent permitted by
+.br
 law.
 
 .SH SEE ALSO
-.IP \fBpni-tools\fR 
+For more information consult the info page for the \fBpni-tools\fR program 
+suite \fBinfo pni-tools\fR.
 
 
 
