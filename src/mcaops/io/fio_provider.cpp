d9d0f6567d12efedfb879d3a6e2582297a7ef85f
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-05-29 12:29:35 +0200

    Added data provider for FIO files

---
+++ src/mcaops/io/fio_provider.cpp
@@ -0,0 +1,148 @@
+//
+// (c) Copyright 2015 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of pnitools.
+//
+// libpniutils is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpniutils is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+// Created on: May 29, 2015
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include "fio_provider.hpp"
+#include "index_generator.hpp"
+#include <pni/io/exceptions.hpp>
+
+using namespace pni::core;
+using namespace pni::io;
+
+fio_provider::fio_provider(const string &filename, 
+                           const string &mca_column_name,
+                           const string &channel_column_name):
+    data_provider(),
+    _filename(filename),
+    _mca_column(mca_column_name),
+    _channel_column(channel_column_name),
+    _file_read(false)
+{
+    if(_mca_column.empty())
+        throw key_error(EXCEPTION_RECORD,
+                "For FIO files the column with MCA data must be provided!");
+}
+
+//----------------------------------------------------------------------------
+fio_provider :: ~fio_provider()
+{}
+
+//----------------------------------------------------------------------------
+bool fio_provider::finished() const
+{
+    return _file_read;
+}
+
+//----------------------------------------------------------------------------
+fio_provider::array_type 
+fio_provider::get_mca_data(const fio_reader &reader) const
+{
+    try
+    {
+        auto mca = reader.column<vector_type>(_mca_column);
+        shape_t shape{mca.size()};
+
+        return array_type::create(shape,std::move(mca));
+
+    }
+    catch(key_error &error)
+    {
+        std::cerr<<error<<std::endl;
+    }
+    catch(file_error &error)
+    {
+        std::cerr<<error<<std::endl;
+    }
+    catch(...)
+    {
+        std::cerr<<"Unknown error when readint MCA data!"<<std::endl;
+    }
+
+    //if we arrive here something went wrong
+    throw io_error(EXCEPTION_RECORD,
+            "Failed to read MCA data from the file!");
+
+}
+
+//----------------------------------------------------------------------------
+fio_provider::array_type
+fio_provider::get_channel_data(const fio_reader &reader,size_t n) const
+{
+    vector_type channels;
+    if(!_channel_column.empty())
+    {
+        try
+        {
+            //read channel/bin-center data from the file
+             channels = reader.column<vector_type>(_channel_column);
+            shape_t shape{channels.size()};
+
+            return array_type::create(shape,channels);
+        }
+        catch(key_error &error)
+        {
+            std::cerr<<error<<std::endl;
+        }
+        catch(file_error &error)
+        {
+            std::cerr<<error<<std::endl;
+        }
+        catch(...)
+        {
+            std::cerr<<"Unkown error when reading channel data!"<<std::endl;
+        }
+
+        throw io_error(EXCEPTION_RECORD,
+                "Failed to read channel data from the file!");
+    }
+    else
+    {
+        //generate artificial channel index
+        std::generate_n(std::back_inserter(channels),n,index_generator());
+        return array_type::create(shape_t{n},std::move(channels));
+    }
+
+}
+
+//----------------------------------------------------------------------------
+data_provider::value_type fio_provider::next() 
+{
+    //change the state
+    _file_read = true;
+   
+    //open the file
+    fio_reader reader(_filename);
+
+    //read MCA data from the file
+    array_type mca = get_mca_data(reader);
+
+    //read or generate channel/bin-center data
+    array_type channels = get_channel_data(reader,mca.size());
+
+    //finally construct the result 
+    return value_type{std::move(mca),std::move(channels)};
+}
+
+//----------------------------------------------------------------------------
---
+++ src/mcaops/io/fio_provider.hpp
@@ -0,0 +1,115 @@
+//
+// (c) Copyright 2015 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of pnitools.
+//
+// libpniutils is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpniutils is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+// Created on: May 29, 2015
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+#pragma once
+
+#include "data_provider.hpp"
+#include <pni/io/fio/fio_reader.hpp>
+
+//!
+//! \ingroup mcaops_devel
+//! \brief read MCA data from a FIO file
+//! 
+//! This provider reads MCA data from a FIO file. As FIO files can store
+//! several columns of data the user must provide at least the name of the 
+//! column from which to read the MCA data. So the -m option is mandatory 
+//! when using FIO files. If no column with the channel or bin-center data
+//! is provided the channel index will be computed automatically. 
+//! If the user also provides the name of the column with the bin or 
+//! channel index data this data will be used instread.
+//!
+class fio_provider : public data_provider
+{
+    private:
+        //some private data types
+        typedef operation::array_type array_type;
+        typedef std::vector<pni::core::float64> vector_type;
+
+        //! stores the name of the file from which to read
+        pni::core::string _filename;
+        //! column name from which to read MCA data
+        pni::core::string _mca_column;
+        //! column name from which to read channel/center bin data
+        pni::core::string _channel_column;
+        //! flag indicating whether or not a file has been read
+        bool _file_read;
+        
+        //!
+        //! \brief read mca data
+        //! 
+        //! \throws io_error in case of any IO error
+        //! \param reader reference to the reader instance
+        //! \return array with MCA data
+        //!
+        array_type get_mca_data(const pni::io::fio_reader &reader) const;
+
+        //--------------------------------------------------------------------
+        //!
+        //! \brief get channel data
+        //!
+        //! The last argument is the number of channels. This value has to be
+        //! provided for situations where no channel column is provided by the 
+        //! user.
+        //! 
+        //! \throws io_error in case of errors
+        //! \param reader reference to the reader instance
+        //! \param n number of channels
+        //! \return channel/center-bin data
+        //! 
+        array_type get_channel_data(const pni::io::fio_reader &reader,
+                                    size_t n) const;
+
+    public:
+        //! 
+        //! \brief constructor
+        //!
+        //! \throws key_error if no MCA column name has been provided
+        //! 
+        //! \param filename the name of the file from which to read data
+        //! \param mca_column_name column name for MCA data
+        //! \param channel_column_name column name for channel/center-bin data
+        //!
+        fio_provider(const pni::core::string &filename,
+                     const pni::core::string &mca_column_name,
+                     const pni::core::string &channel_column_name="");
+
+        //--------------------------------------------------------------------
+        //! destructor
+        ~fio_provider();
+
+        //--------------------------------------------------------------------
+        //!
+        //! \brief true if finished
+        //!
+        //! \return true if data has been delivered 
+        //!
+        virtual bool finished() const;
+
+        //--------------------------------------------------------------------
+        //!
+        //! \brief next dataset
+        //! 
+        //! \throws io_error in case of any IO error
+        //!
+        //! \return return the next dataset
+        //!
+        virtual value_type next();
+};
